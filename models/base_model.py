53c8a9ea91370c3beff5350b2e98a429be1d3cea
Biyahon <biyahon@gmail.com>
2023-03-26 23:08:21 -0700

    revised commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,56 +1,78 @@
 #!/usr/bin/python3
-"""This script is the base model"""
+"""
+class Base model
+"""
 
-import uuid
+
+from uuid import uuid4
 from datetime import datetime
-from models import storage
+import models
+
+
+def time_conversor(obj):
+    """ Define time conversor
+        that return new time object
+    """
+    if type(obj) in [datetime]:
+        obj = obj.strftime('%Y-%m-%dT%H:%M:%S.%f')
+    return datetime.strptime(obj, "%Y-%m-%dT%H:%M:%S.%f")
 
 
 class BaseModel:
-
-    """Class from which all other classes will inherit"""
-
+    """ Class Base Model
+    """
     def __init__(self, *args, **kwargs):
-        """Initializes instance attributes
-
-        Args:
-            - *args: list of arguments
-            - **kwargs: dict of key-values arguments
+        """ Initialize method init
+            Base instance
         """
-
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        if kwargs:
+            self.created_at = time_conversor(kwargs["created_at"])
+            self.updated_at = time_conversor(kwargs["updated_at"])
+            for k, v in kwargs.items():
+                if k == '__class__':
+                    continue
                 else:
-                    self.__dict__[key] = kwargs[key]
+                    setattr(self, k, v)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+            self.id = str(uuid4())
+            self.created_at = datetime.today().isoformat()
+            self.updated_at = datetime.today().isoformat()
+            models.storage.new(self)
 
     def __str__(self):
-        """Returns official string representation"""
+        """ Define method str that return
+            string representation
+        """
+        self.__dict__.update({
+            "created_at": time_conversor(self.created_at),
+            "updated_at": time_conversor(self.updated_at),
+        })
+        return "[{:s}] ({:s}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
-        return "[{}] ({}) {}".\
-            format(type(self).__name__, self.id, self.__dict__)
+    def __repr__(self):
+        """ Define method repr that return
+            string representation
+        """
+        return "[{:s}] ({:s}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """updates the public instance attribute updated_at"""
-
-        self.updated_at = datetime.now()
-        storage.save()
+        """ method save that calls storage
+        """
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__"""
-
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = type(self).__name__
-        my_dict["created_at"] = my_dict["created_at"].isoformat()
-        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
-        return my_dict
+        """ Method to dictionary
+            that return dict representation
+        """
+        if type(self.created_at) in [str]:
+            self.created_at = time_conversor(self.created_at)
+        if type(self.updated_at) in [str]:
+            self.updated_at = time_conversor(self.updated_at)
+        self.created_at = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        self.updated_at = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        aux_val = (self.__dict__).copy()
+        aux_val['__class__'] = self.__class__.__name__
+        return aux_val
